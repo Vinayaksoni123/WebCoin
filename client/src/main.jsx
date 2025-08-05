5c3e2401f7377be1f1211471699b0dc269327d0d
vinayak soni <147817292+Vinayaksoni123@users.noreply.github.com>
2025-08-05 18:51:03 +0530

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { AuthProvider } from "./Context/Authprovider.jsx";
+import { AuthProvider } from "./Context/AuthProvider.jsx";
 import { ToastContainer } from "react-toastify";
 import { BrowserRouter } from "react-router";
 export const serverUrl = "https://webcoin-7yl6.onrender.com";
